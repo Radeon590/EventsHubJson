c72b37174beff93d36257aea6d9c606648646ebb
Rodion <59920979+Radeon590@users.noreply.github.com>
2024-05-09 13:04:28 +0300

    client sign in and get user

--- events-hub-client/src/containers/signin.js
+++ events-hub-client/src/containers/signin.js
@@ -1,6 +1,33 @@
+import { useRef } from "react";
+import "./signin.css";
+
 function SignIn(){
+  const usernameInput = useRef(null);
+  const passwordInput = useRef(null);
+
+  async function signInAsUser(){
+    let result = await fetch(`http://localhost:5141/api/Users/Authorize?username=${usernameInput.current.value}&password=${passwordInput.current.value}`, {
+      method: "GET",
+      credentials: "include"
+    });
+    console.log(result.status);
+    console.log(result.statusText);
+    if (result.status === 200){
+      let readUserResult = await fetch(`http://localhost:5141/api/Users/ReadByUsername?username=${usernameInput.current.value}`, {
+        method: "GET",
+        credentials: "include"
+      });
+      console.log(readUserResult.json());
+    }
+  }
+
     return(
-      <div><h1>signIn</h1></div>  
+      <div className="input-group">
+        <h1>Sign In as user</h1>
+        <input type="text" placeholder="username" ref={usernameInput}/>
+        <input type="password" placeholder="password" ref={passwordInput}/>
+        <button onClick={signInAsUser}>Sign in</button>
+      </div>  
     );
 }
 
