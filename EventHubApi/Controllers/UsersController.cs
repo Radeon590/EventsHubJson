176862341033e63af55cc099f2ac7f4be1a32bfb
Rodion <59920979+Radeon590@users.noreply.github.com>
2024-05-10 18:05:45 +0300

    api create user from json

--- EventHubApi/Controllers/UsersController.cs
+++ EventHubApi/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 using System;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EventsHubApi.Controllers
 {
@@ -27,18 +28,36 @@ namespace EventsHubApi.Controllers
         [Route("Create")]
         public async Task<IResult> Create(string username, string useremail, string password, string firstname, string secondname, string patronymic, int age)
         {
+            //
+            User newUser = new User(username, useremail, password, firstname, secondname, patronymic, age);
+            return await Create(newUser);
+        }
+
+        [HttpPost]
+        [Route("CreateFromJson")]
+        public async Task<IResult> Create()
+        {
+            User? newUser = await HttpContext.Request.ReadFromJsonAsync<User>();
+            if (newUser != null) 
+            {
+                return await Create(newUser);
+            }
+            return Results.BadRequest("cant get user json from request body");
+        }
+
+        private async Task<IResult> Create(User user)
+        {
             //check unique
-            int countNotUnique = await _applicationContext.Users.Where(u => u.Username == useremail || u.UserEmail == useremail).CountAsync();
-            if (countNotUnique > 0) 
+            int countNotUnique = await _applicationContext.Users.Where(u => u.Username == user.Username || u.UserEmail == user.UserEmail).CountAsync();
+            if (countNotUnique > 0)
             {
                 return Results.Conflict("user with this username ot useremail is exists");
             }
             //
-            User newUser = new User(username, useremail, password, firstname, secondname, patronymic, age);
-            await _applicationContext.Users.AddAsync(newUser);
+            await _applicationContext.Users.AddAsync(user);
             await _applicationContext.SaveChangesAsync();
             //TODO: check return id
-            return Results.Ok(newUser.Id);
+            return Results.Ok(user.Id);
         }
 
         [HttpGet]
