97b7ea6761858c8b4ad138ec0e2674ecf42d3f44
Rodion <59920979+Radeon590@users.noreply.github.com>
2024-05-10 17:54:44 +0300

    client UserSignUp

--- events-hub-client/src/components/UserSignUp.js
+++ events-hub-client/src/components/UserSignUp.js
@@ -1,9 +1,54 @@
 import "../css/input-group.css";
+import { createUserAccount } from "../store/actionCreators/account";
+import { connect } from "react-redux";
+import { useRef } from "react";
 
-export default function UserSignUp(){
-    return(
+function UserSignUp({ onSignUp, createUserAccount }) {
+    const usernameInput = useRef(null);
+    const useremailInput = useRef(null);
+    const passwordInput = useRef(null);
+    const firstnameInput = useRef(null);
+    const secondnameInput = useRef(null);
+    const patronymicInput = useRef(null);
+    const ageInput = useRef(null);
+
+    function handleSignUp() {
+        const userData = {
+            username: usernameInput.current.value,
+            useremail: useremailInput.current.value,
+            password: passwordInput.current.value,
+            firstname: firstnameInput.current.value,
+            secondname: secondnameInput.current.value,
+            patronymic: patronymicInput.current.value,
+            age: ageInput.current.value
+        }
+        createUserAccount(userData);
+        onSignUp();
+    }
+
+    return (
         <div className="input-group">
-          <h1>Sign Up as user</h1>
-        </div>  
-      );
+            <h1>Sign Up as user</h1>
+            <input type="text" placeholder="username" ref={usernameInput} />
+            <input type="email" placeholder="useremail" ref={useremailInput} />
+            <input type="password" placeholder="password" ref={passwordInput} />
+            <input type="text" placeholder="firstname" ref={firstnameInput} />
+            <input type="text" placeholder="secondname" ref={secondnameInput} />
+            <input type="text" placeholder="patronymic" ref={patronymicInput} />
+            <input type="number" placeholder="age" ref={ageInput} />
+            <button onClick={handleSignUp}>Sign Up</button>
+        </div>
+    );
 }
+
+const mapStateToProps = state => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = {
+    createUserAccount
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserSignUp);
